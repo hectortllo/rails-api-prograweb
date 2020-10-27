1a63c8a4aef14066b4570e8ae02dfc650c4b51d1
Héctor Tello <hectortllo@gmail.com>
2020-10-27 17:43:28 -0600

    Se activaron los CORS

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,14 @@ module ApiPrograweb
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        headers: :any,
+        methods: [:get, :patch, :put, :post]
+      end
+    end
   end
 end
