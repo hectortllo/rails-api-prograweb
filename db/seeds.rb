9dc4d5280036e34cc32fb41ab986eea3b7802bfd
Héctor Tello <hectortllo@gmail.com>
2020-11-02 11:52:42 -0600

    Se agregó el archivo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Place.create(
+  name: "Tikal",
+  description: "Tikal es una antigua ciudadela maya en los bosques tropicales del norte de Guatemala",
+  location: "Petén, Guatemala",
+  score: 5,
+  image: File.open('./img/tikal.jpg')
+)
+Place.create(
+  name: "Lago de Atitlán",
+  description: "El lago de Atitlán es uno de los atractivos turísticos de Guatemala más visitados",
+  location: "Sololá, Guatemala",
+  score: 5,
+  image: File.open('./img/atitlan.jpg')
+)
+Place.create(
+  name: "Lago de Amatitlán",
+  description: "El lago de Amatitlán es un lago de cráter situado en Guatemala",
+  location: "Guatemala, Guatemala",
+  score: 5,
+  image: File.open('./img/amatitlan.jpg')
+)
+Place.create(
+  name: "Tikal",
+  description: "Tikal es una antigua ciudadela maya en los bosques tropicales del norte de Guatemala",
+  location: "Petén, Guatemala",
+  score: 5,
+  image: File.open('./img/tikal.jpg')
+)
+Place.create(
+  name: "Lago de Atitlán",
+  description: "El lago de Atitlán es uno de los atractivos turísticos de Guatemala más visitados",
+  location: "Sololá, Guatemala",
+  score: 5,
+  image: File.open('./img/atitlan.jpg')
+)
+Place.create(
+  name: "Lago de Amatitlán",
+  description: "El lago de Amatitlán es un lago de cráter situado en Guatemala",
+  location: "Guatemala, Guatemala",
+  score: 5,
+  image: File.open('./img/amatitlan.jpg')
+)
